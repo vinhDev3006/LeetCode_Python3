8f662447e4288b730d268a92fb04b518ea3e024d
Nguyễn Ngọc Vĩnh <122826039+vinhDev3006@users.noreply.github.com>
2023-04-01 12:34:13 +0700

    Create Problem1450_NumberOfStudentsDoingHomeworkAtAGivenTime.py

---
+++ Problem1450_NumberOfStudentsDoingHomeworkAtAGivenTime.py
@@ -0,0 +1,43 @@
+"""
+Problem 1450: Number of Students Doing Homework at a Given Time
+URL: https://leetcode.com/problems/number-of-students-doing-homework-at-a-given-time/
+Runtime: 35 ms, Beat: 83.51%
+Memory: 13.8 MB, Beat: 60.40%
+Description:
+Given two integer arrays startTime and endTime and given an integer queryTime.
+The ith student started doing their homework at the time startTime[i] and finished it at time endTime[i].
+Return the number of students doing their homework at time queryTime. More formally, return the number of students
+where queryTime lays in the interval [startTime[i], endTime[i]] inclusive.
+
+Example 1:
+Input: startTime = [1,2,3], endTime = [3,2,7], queryTime = 4
+Output: 1
+Explanation: We have 3 students where:
+The first student started doing homework at time 1 and finished at time 3 and wasn't doing anything at time 4.
+The second student started doing homework at time 2 and finished at time 2 and also wasn't doing anything at time 4.
+The third student started doing homework at time 3 and finished at time 7 and was the only student doing homework at time 4.
+"""
+import unittest
+from typing import List
+
+
+class Solution:
+    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+        count = 0
+        for i in range(len(startTime)):
+            if startTime[i] <= queryTime <= endTime[i]:
+                count += 1
+        return count
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self) -> None:
+        self.solution = Solution()
+        self.testCase1 = ([1, 2, 3], [3, 2, 7], 4)
+
+    def test_test_case_1(self):
+        self.assertEqual(1, self.solution.busyStudent(self.testCase1[0], self.testCase1[1], 4))
+
+
+if __name__ == '__main__':
+    unittest.main()
